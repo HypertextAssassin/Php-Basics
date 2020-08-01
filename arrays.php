174996d96f2f70f54f2abc8ad36d071ae56fd6b8
HypertextAssassin <mridul2598@gmail.com>
2020-08-01 10:44:29 +0530

    Uploaded

---
+++ arrays.php
@@ -0,0 +1,19 @@
+<?php
+// $frns = array("anand","Kunal","Mridul");
+// echo $frns.' ';
+// echo $frns[1];
+// $arr = array(1,2,3,4,5);
+// $arr[] = 6;
+// $arr[] = 7;
+// print_r($arr);
+//Associative arrays (Key => Value pair)
+$ages = array('Mridul' => 22 , 'Anand' => 23 , 'Kunal' => 21);
+echo $ages['Mridul'];
+// print_r($ages);
+var_dump($ages);
+//2D arrays
+$arr2d = array(array(00,01,02),array(10,11,12),array(20,21,22));
+print_r($arr2d);
+$arrType = array('mridul' , true , 4);
+print_r($arrType);
+?>
---
+++ variables.php
@@ -0,0 +1,11 @@
+<?php 
+$num1 = 'Hello';
+$num2 = ' World';
+$num3;
+echo $num3;
+$num3 = "$num1  $num2";
+echo $num3;
+$diag = "They're here";
+echo $diag;
+
+?>
